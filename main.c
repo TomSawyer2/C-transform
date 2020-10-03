823916c641f474370a24e2683f8e77c19aedc17b
unknown <6272867@qq.com>
2020-10-03 11:24:36 +0800

    早上进行了基本的解析，判断函数的开始与结束。

--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 #include "isletter.h"
 #include "isdigit.h"
 #include "binary.h"
+#include "cJSON.h"
 //保留字表
-char keywords[15][12]={"int","float","double","break","else","char","return","continue","for","if","while","void"};    
+char keywords[15][12]={"int","float","double","printf","scanf","getchar","break","else","char","return","continue","for","if","while","void"};    
 //分隔符表
 char delimiters[20][5]={"=","+","-","&","*","**",",",";","(",")","{","}","<",">","#","[","]","."};   
 //标识符表
@@ -21,7 +22,8 @@ int main(void){
 
     FILE *file,*file1;
     file1=fopen("词法分析器（结果）.txt","w");
-    char string[300];
+    //malloc(400*sizeof(int));
+    char string[380];
     int j0,k0=0;
     file=fopen("词法分析器.txt","r");//打开文件 
     char ch0;
@@ -29,7 +31,7 @@ int main(void){
         string[k0++]=ch0;
     }
     string[k0]='\0';
-    char str[300]={};
+    char str[380]={};
     strcpy(str,string);
     str[k0]='\0';
 
@@ -37,7 +39,8 @@ int main(void){
     int length=getLength(str);
     //printf("length=%d",length);
     printf("成功，请前往文件:词法分析器（结果）.txt中查看！"); 
-    fprintf(file1,"类别码  值\n");                                                                                       
+    fprintf(file1,"类别码  值   操作\n");
+	fprintf(file1,"                 开始\n");                                                                                       
     for(i=0;i<length;i++){
         char strToken[]={""};
         ch=str[i];
@@ -53,12 +56,29 @@ int main(void){
             int keyword=IsKeyword(strToken);    //判断是否为保留字 
             if(keyword==0){      //为标识符 
                 InsertId(strToken);
-                fprintf(file1,"31   %s\n",strToken);      
+                fprintf(file1,"34   %s\n",strToken);      
             }
-            else{       //为保留字 
-                fprintf(file1,"%d    %s\n",keyword,strToken); 
-            }
-        }
+            else{//为保留字
+			    if(keyword==4){
+			    	fprintf(file1,"%d    %s    输出\n",keyword,strToken); 
+				}else if(keyword==5||keyword==6){
+					fprintf(file1,"%d    %s    输入\n",keyword,strToken);
+					}else if(keyword==10){
+						while(str[i+1]!=';'){
+							i++;
+						}fprintf(file1,"sp3      函数结束\n");
+						i++;
+						}else{
+							fprintf(file1,"%d    %s\n",keyword,strToken);
+						}
+					}
+				}        
+        else if(str[i]&0x80 && str[i+1]&0x80){//为汉字 
+        	
+        	//fprintf(file1,"35      %c%c\n",str[i],str[i+1]);
+        	
+        	i+=2; 
+		}
         else if(IsDigit(ch)){   //为数字 
             while(IsDigit(ch)){
                 Concat(getLength(strToken),strToken,ch);
@@ -67,7 +87,7 @@ int main(void){
             }
             i--;
             InsertDigit(Binary(atoi(strToken)));    //将strToken字符串转化为数字插入到常数表中 
-            fprintf(file1,"32   %d\n",atoi(strToken));
+            fprintf(file1,"35   %d\n",atoi(strToken));
         }
         else if(ch=='='){
             fprintf(file1,"%d    =\n",IsDelimiter(ch));
@@ -82,10 +102,10 @@ int main(void){
             i++;
             ch=str[i];
             if(ch=='*'){
-                fprintf(file1,"17   **\n");
+                fprintf(file1,"20   **\n");
 
             }else{
-                fprintf(file1,"16   *\n");
+                fprintf(file1,"19   *\n");
                 i--;
             } 
         }
@@ -158,10 +178,10 @@ int main(void){
 			 }
         }
         else if(ch=='"'){
-        	fprintf(file1,"33    \"\n");
+        	fprintf(file1,"36    \"\n");
 		}
 		else if(ch=='%'){
-			fprintf(file1,"34    %%\n");
+			fprintf(file1,"37    %%\n");
 		} 
         else{
             fprintf(file1,"错误！\n"); 
@@ -206,7 +226,7 @@ int IsDelimiter(char *ch){
     int h;
     for(h=0;h<20;h++){
         if(delimiters[h][0]==ch){
-            return h+13;     //若是保留字，则返回它的类别码 
+            return h+16;     //若是保留字，则返回它的类别码 
         }
     }
     return 0;
